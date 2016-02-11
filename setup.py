e1e2d105735c71e96791216b89862675022fd13c
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-02-11 10:41:07 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='Flask-pyoidc',
+        version='0.0.1',
+        packages=find_packages('src'),
+        package_dir={'': 'src'},
+        url='https://github.com/its-dirg/flask-pyoidc',
+        license='Apache 2.0',
+        author='Rebecka Gulliksson',
+        author_email='rebecka.gulliksson@umu.se',
+        description='Flask extension for OpenID Connect authentication.',
+        install_requires=[
+            'oic',
+            'Flask'
+        ]
+)
