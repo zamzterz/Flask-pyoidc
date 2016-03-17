4baeec172877edbf1909edb65cff0c04fc884b0a
Rebecka Gulliksson <rebecka.gulliksson@umu.se>
2016-03-17 20:44:07 +0100

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='Flask-pyoidc',
-        version='0.1.1',
+        version='0.1.2',
         packages=find_packages('src'),
         package_dir={'': 'src'},
         url='https://github.com/its-dirg/flask-pyoidc',
