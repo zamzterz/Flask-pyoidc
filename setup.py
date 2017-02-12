d37397841a0800241c05417d42ac528ec31bc0bf
Samuel Gulliksson <samuel.gulliksson@gmail.com>
2017-02-12 10:34:30 +0100

    Update author information.

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
         version='1.0.0',
         packages=find_packages('src'),
         package_dir={'': 'src'},
-        url='https://github.com/its-dirg/flask-pyoidc',
+        url='https://github.com/zamzterz/flask-pyoidc',
         license='Apache 2.0',
-        author='Rebecka Gulliksson',
-        author_email='rebecka.gulliksson@umu.se',
+        author='Samuel Gulliksson',
+        author_email='samuel.gulliksson@gmail.com',
         description='Flask extension for OpenID Connect authentication.',
         install_requires=[
             'oic==0.9.1.0',
