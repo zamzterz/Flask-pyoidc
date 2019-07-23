d47407e12a9d77de9a87d8a83151dcdf4cd543af
Samuel Gulliksson <samuel.gulliksson@gmail.com>
2019-07-23 22:16:35 +0200

    Use README as package description.

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+with open('README.md') as f:
+    long_description = f.read()
 
 setup(
-        name='Flask-pyoidc',
-        version='2.1.0',
-        packages=['flask_pyoidc'],
-        package_dir={'': 'src'},
-        url='https://github.com/zamzterz/flask-pyoidc',
-        license='Apache 2.0',
-        author='Samuel Gulliksson',
-        author_email='samuel.gulliksson@gmail.com',
-        description='Flask extension for OpenID Connect authentication.',
-        install_requires=[
-            'oic==0.12',
-            'Flask',
-            'requests'
-        ],
-        package_data={'flask_pyoidc': ['files/parse_fragment.html']},
+    name='Flask-pyoidc',
+    version='2.1.0',
+    packages=['flask_pyoidc'],
+    package_dir={'': 'src'},
+    url='https://github.com/zamzterz/flask-pyoidc',
+    license='Apache 2.0',
+    author='Samuel Gulliksson',
+    author_email='samuel.gulliksson@gmail.com',
+    description='Flask extension for OpenID Connect authentication.',
+    install_requires=[
+        'oic==0.12',
+        'Flask',
+        'requests'
+    ],
+    package_data={'flask_pyoidc': ['files/parse_fragment.html']},
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
