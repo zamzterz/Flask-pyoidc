a85d2b11c85107c827ae01120588f1f746bf9b30
Samuel Gulliksson <samuel.gulliksson@gmail.com>
2019-12-04 16:54:38 +0100

    Bump version: 3.0.0 → 3.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='Flask-pyoidc',
-    version='3.0.0',
+    version='3.1.0',
     packages=['flask_pyoidc'],
     package_dir={'': 'src'},
     url='https://github.com/zamzterz/flask-pyoidc',
