dd836b01cce21a07faad39d0e3238f610726b086
Samuel Gulliksson <zamzterz@users.noreply.github.com>
2020-04-29 19:57:32 +0200

    Fix #78: Allow domain name to be configured without Flask SERVER_NAME. (#79)

--- tests/test_flask_pyoidc.py
+++ tests/test_flask_pyoidc.py
@@ -501,3 +501,15 @@ class TestOIDCAuthentication(object):
         authn = self.init_app()
         assert authn._redirect_uri_endpoint == 'openid_connect_login'
         assert authn.clients['test_provider']._redirect_uri == 'http://client.example.com/openid_connect_login'
+
+    def test_should_use_custom_redirect_domain(self):
+        self.app.config['PREFERRED_URL_SCHEME'] = 'https'
+        self.app.config['OIDC_REDIRECT_DOMAIN'] = 'custom.example.com'
+        authn = self.init_app()
+        assert authn.clients['test_provider']._redirect_uri == 'https://custom.example.com/redirect_uri'
+
+    def test_should_raise_if_domain_not_specified(self):
+        self.app.config['SERVER_NAME'] = None
+        with pytest.raises(ValueError) as exc_info:
+            self.init_app()
+        assert 'OIDC_REDIRECT_DOMAIN' in str(exc_info.value)
